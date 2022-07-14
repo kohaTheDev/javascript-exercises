464b820d0f775af9d58e60b794328a5cb399db6d
koHa <khoa.soft.e@gmail.com>
2022-07-14 18:20:02 +0700

    completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(begin, end) {
+    for (const argument of arguments) {
+        if (argument < 0 ||  !Number.isInteger(argument)) {
+            return 'ERROR';
+        }
+    }
+    let sum = (end + begin) * (Math.abs(end - begin) + 1) / 2;
+    return sum;
 };
 
 // Do not edit below this line
