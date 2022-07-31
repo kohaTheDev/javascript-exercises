bfa1904468df1b0e46dfe4660ed72aa9ba59b3f5
koHa <khoa.soft.e@gmail.com>
2022-07-31 21:51:19 +0700

    completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    str = str.replace(/[^a-z0-9]/gi, '');
+    let newStr = str.split("").reverse().join("");
+    return str === newStr;
 };
 
 // Do not edit below this line
