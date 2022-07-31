692447db49a688134d055af989ed7586acdc8a9e
koHa <khoa.soft.e@gmail.com>
2022-07-31 21:59:05 +0700

    completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+let fibo = [1,1];
+const fibonacci = function(x) {
+    if(x < 0 ) return "OOPS";
+    if (fibo[x-1] === undefined) {
+        fibo[x-1] = fibonacci(x-2) + fibonacci(x-1);
+    }
+    return fibo[x-1];
 };
 
 // Do not edit below this line
